30524ccffd2926165b766d7fb9136736df237ae6
PapaIPapa <Chechnya69@yandex.ru>
2024-10-15 17:08:11 +0300

    Добавьте файлы проекта.

---
+++ 3kt/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
+
+namespace _3kt
+{
+
+
+    public class DatabaseDB
+    {
+        private SQLiteConnection _connection;
+
+        public DatabaseDB(string databasePath)
+        {
+            _connection = new SQLiteConnection($"Data Source={databasePath}");
+            _connection.Open();
+        }
+
+        public void CreateTable(string tableName, string columns)
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = $"CREATE TABLE IF NOT EXISTS {tableName} ({columns})";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertData(string tableName, string columns, string values)
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+    }
+}
---
+++ 3kt/Program.cs
@@ -0,0 +1,56 @@
+﻿using _3kt;
+using System.Collections.Generic;
+using System;
+using Bogus;
+using Microsoft.Extensions.Primitives;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var faker = new Faker();
+
+        var people = new List<Person>();
+        for (int i = 0; i < 10; i++)
+        {
+            var person = new Person
+            {
+                Name = faker.Name.FirstName() + " " + faker.Name.LastName(),
+                Email = faker.Internet.Email(),
+                Age = faker.Random.Number(1, 100)
+            };
+            people.Add(person);
+            Console.WriteLine(person.Name + " " + person.Email + " " + person.Age);
+        }
+
+        
+
+        string databasePath = "mydatabase.db";
+        string tableName = "People";
+        string columns = "Name TEXT, Email TEXT, Age INTEGER";
+        string columnsINSERT = "Name, Email, Age";
+
+
+        DatabaseDB database = new DatabaseDB(databasePath);
+      
+        database.CreateTable(tableName, columns);
+        foreach (var pers in people)
+        {
+            if (pers.Age >= 14)
+            {
+                string values = $" '{pers.Name}' , '{pers.Email}' , {pers.Age}";
+                database.InsertData(tableName, columnsINSERT, values);
+            }
+            else { Console.WriteLine("маленький еще"); }
+        }
+        Console.WriteLine("ура, все!!!");
+        Console.ReadLine();
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
